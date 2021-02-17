8b695356912a2ba4156b778053b7bd0cdb2a2b03
LeeGof <ligfufida@gmail.com>
2021-02-17 11:16:52 +0800

    commit

---
+++ Example/GofMediatorExtension/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GofMediatorExtension
+//
+//  Created by LeeGof on 02/16/2021.
+//  Copyright (c) 2021 LeeGof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
